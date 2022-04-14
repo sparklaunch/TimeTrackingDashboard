c7a1bd4eff9026931f92e270e15be2d585ac6fdf
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-14 13:00:04 +0900

    Initial Commit

---
+++ Shared/TimeTrackingDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackingDashboardApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/14.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackingDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
